8dd3c7393c90c61503b3ed502d238ade3e3fdfea
Thuan <Thuanvodv@gmail.com>
2022-10-29 11:46:41 +0700

    pagination

--- controllers/prod.controller.js
+++ controllers/prod.controller.js
@@ -1,7 +1,25 @@
 import db from '../db.js';
+import _ from 'lodash';
 function index(req, res) {
+    var perPage = 4;
+    var page = (req.query.page) || 1;
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
+    var preV, next;
+    if (page > 1) {
+        preV = page - 1;
+        next = ++page;
+    }
+    else {
+        preV = 1;
+        next = 2;
+    }
+    if (next > parseInt(db.data.products.length / 4)) next = parseInt(db.data.products.length / 4)
     res.render('prod/index', {
-        users: db.data.products
+        products: db.data.products.slice(start, end),
+        page: page,
+        preV: preV,
+        next: next
     });
 };
 export default index;
